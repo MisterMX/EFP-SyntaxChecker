9badb1781845a92613c3879948474166973c10da
MisterMX <mbxd12@web.de>
2017-05-17 11:25:31 +0200

    Setup ring server

--- project.clj
+++ project.clj
@@ -1,9 +1,28 @@
-(defproject efp-syntaxchecker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject compojure "0.1.0-SNAPSHOT"
+  :description "A concise routing library for Ring"
+  :url "https://github.com/weavejester/compojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot efp-syntaxchecker.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [clout "2.1.2"]
+                 [medley "1.0.0"]
+                 [ring "1.6.1"]
+                 [ring/ring-codec "1.0.1"]
+                 [compojure "1.6.0"]]
+  :plugins [[lein-codox "0.10.3"]
+            [lein-ring "0.12.0"]]
+  :ring {:handler efp_syntaxchecker.core/app}
+  :codox
+  {:output-path "codox"
+   :metadata {:doc/format :markdown}
+   :source-uri "http://github.com/weavejester/compojure/blob/{version}/{filepath}#L{line}"}
+  :aliases
+  {"test-all" ["with-profile" "default:+1.8" "test"]}
+  :profiles
+  {:dev {
+    :jvm-opts ^:replace []
+    :dependencies [[ring/ring-mock "0.3.0"]
+                  [criterium "0.4.4"]
+                  [javax.servlet/servlet-api "2.5"]]}
+    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
--- src/efp_syntaxchecker/core.clj
+++ src/efp_syntaxchecker/core.clj
@@ -1,7 +1,7 @@
-(ns efp-syntaxchecker.core
-  (:gen-class))
+(ns efp_syntaxchecker.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
