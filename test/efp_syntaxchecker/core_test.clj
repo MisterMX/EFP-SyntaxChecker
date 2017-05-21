afaa83e324b3eebfd44a91a58f735b7092a232c6
Max Blatt <max.blatt@web.de>
2017-05-21 22:24:26 +0200

    Setup VS Code debug environment

--- test/efp_syntaxchecker/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns efp-syntaxchecker.core-test
-  (:require [clojure.test :refer :all]
-            [efp-syntaxchecker.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
