11ead269013f275dc9aa4f6793d060347d754ceb
MisterMX <mbxd12@web.de>
2017-05-29 20:55:32 +0200

    Support HTTPS via jetty

--- src/efp_syntaxchecker/core.clj
+++ src/efp_syntaxchecker/core.clj
@@ -1,11 +1,24 @@
 (ns efp_syntaxchecker.core
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]))
+  (:use compojure.core
+        ring.adapter.jetty)
+  (:require [compojure.route :as route]))
 
-(defn getText
-  [request]
-  (str request "test"));
+(defn getText [request]
+  (str request " test"))
 
-(defroutes app
-  (GET "/" [request] (getText [request]))
-  (route/not-found "<h1>Page not found</h1>"))
+(defroutes app-routes
+  (POST "/api" [request] (getText [request]))
+  (route/not-found "Page not found"))
+
+(defn start-server [port]
+  (run-jetty app-routes
+    { :port port
+      :ssl? true
+      :ssl-port 8081
+      :keystore "/opt/certificate/keystore"
+      :key-password "efpss17"}))
+
+(defn -main [& [port]]
+  (if port
+    (start-server (Integer/parseInt port))
+    (start-server 8080)))
