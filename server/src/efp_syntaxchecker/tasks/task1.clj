29f067ad7dbd54c855162182d0976878ab2cd55f
MisterMX <mbxd12@web.de>
2017-06-05 16:01:21 +0200

    Add task config and service to fetch them

---
+++ server/src/efp_syntaxchecker/tasks/task1.clj
@@ -0,0 +1,9 @@
+(ns efp_syntaxchecker.tasks.task1)
+
+(defn task1Handler
+  [doc]
+  "test")
+
+(defn task2Handler
+  [doc]
+  "test")
---
+++ server/src/efp_syntaxchecker/tasks/task2.clj
@@ -0,0 +1,9 @@
+(ns efp_syntaxchecker.tasks.task2)
+
+(defn task1Handler
+  [doc]
+  "test")
+
+(defn task2Handler
+  [doc]
+  "test")
