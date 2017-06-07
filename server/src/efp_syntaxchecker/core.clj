0d47ef90e2b57b98069ae94608d9569882bb43f8
Philipp Minges <fjnalta@gmail.com>
2017-06-07 16:19:11 +0200

    make multipart-params work

--- server/src/efp_syntaxchecker/core.clj
+++ server/src/efp_syntaxchecker/core.clj
@@ -1,8 +1,12 @@
 (ns efp_syntaxchecker.core
-  (:use compojure.core
-        efp_syntaxchecker.views.main
-        ring.adapter.jetty
-        ring.util.response)
+  (:use [compojure.core]
+        [ring.middleware.params]
+        [ring.middleware.multipart-params]
+        [ring.adapter.jetty]
+        [ring.util.response]
+        [clojure.java.io]
+        [efp_syntaxchecker.views.main])
+
   (:require 
     [efp_syntaxchecker.config-util :as config-util]
     [efp_syntaxchecker.task-execution :as task-execution]
@@ -10,13 +14,9 @@
     [compojure.route :as route]
     [ring.middleware.json :refer :all]
     [ring.middleware.params :refer :all]
+    [ring.middleware.multipart-params :refer :all]
     [ring.util.json-response :refer :all]
-    [clojure.java.io :as io]
     [hiccup.middleware :only (wrap-base-url)]))
-
-(defn write-file [params]
-  (with-open [w (io/writer  "tmp/input.txt" :append true)]
-    (.write w (str params))))
 
 
 (defroutes app-routes
@@ -26,12 +26,13 @@
   ; Moodle request is application/urlencoded -> data is in :params.
   (POST "/api" {params :params {userId :user_id} :body} (response {:user_id userId :params params}))
 
-  ; TODO - multipart-params is always nil or {} 
+  ; HTTP-Form upload is multipart/form-data
+  ; params enthält Map des Uploads - Key = "file" -> 
+  ; {file [{:filename Customer.java, :content-type text/x-java, :tempfile #object[java.io.File 0x25641dd9 /tmp/ring-multipart-8681951062682279797.tmp], :size 1567}]}
   (POST "/upload" {params :params}
-    (def file 
-      (println params))
-    (write-file file)
-    (response (str params)))
+    (println params)
+    (let [file (get params "file")]
+      (response (get file :tempfile))))
 
   (route/not-found "Page not found"))
 ;---
@@ -40,9 +41,9 @@
 
 (def app
   (-> app-routes
-    (wrap-json-body {:keywords? true :bigdecimals? true})
-    (wrap-params)
-    (wrap-json-response)))
+    wrap-params
+    wrap-multipart-params
+    wrap-json-response))
 
 (defn start-server []
   (run-jetty app
