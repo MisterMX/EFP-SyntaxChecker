c81c9e48c0482d270c81d0d17e12e3511aec1a61
Philipp Minges <fjnalta@gmail.com>
2017-06-12 17:10:45 +0200

    change javascript processing logic

--- client/public/js/parseInputData.js
+++ client/public/js/parseInputData.js
@@ -1,5 +1,5 @@
 var jsonUpload = {
-    tasks: [],
+    taskName: "",
     files: []
 };
 
@@ -15,9 +15,41 @@ function readTasks() {
             
             link.appendChild(text);
             link.href = "#";
+            
+            link.onclick = function() {
+                createTriggerTable(opt);
+                // reset the field
+                jsonUpload.taskName = opt;
+            };
             li.appendChild(link);
             list.appendChild(li);
         }
+   })
+}
+
+function createTriggerTable(triggerName) {
+    var body = "<table class='table table-hover' id='triggerTable'>";
+    body += "<thead>";
+    body += "<tr><th>Trigger</th><th>Info</th><th>Success</th></tr>";
+    body += "</thead>";
+    body += "<tbody>";
+    // parse JSON with jQuery
+    $.getJSON("http://localhost:8080/api/tasks", function(data) {
+        for(var i = 0; i < data.length; i++) {
+            if(data[i].name == triggerName) {
+                for(var j = 0; j < data[i].triggers.length; j++) {
+                    body += "<tr>";
+                    body += "<td>" + j+1 + "</td>";
+                    body += "<td>" + data[i].triggers[j] + "</td>";
+                    body += "<td><img src='img/icons/working.png' width='50px' height='50px'</td>";
+                    body += "</tr>";
+                }
+            }            
+        }
+        
+        body += "</tbody>";
+        body += "</table>";
+        document.getElementById("triggerTable").innerHTML = body;
    })
 }
 
@@ -46,48 +78,9 @@ function readMultipleFiles(evt) {
     }
 }
 
-function createTasks() {
-    var body = "<table class='table table-hover' id='tasksTable'>";
-    body += "<thead>";
-    body += "<tr><th>Taskname</th><th>Info</th><th>Success</th><th>Validate</th></tr>";
-    body += "</thead>";
-
-    body += "<tbody>";
-    // parse JSON with jQuery
-    $.getJSON("http://localhost:8080/api/tasks", function(data) {
-        for(var i = 0; i < data.length; i++) {
-            body += "<tr>";
-            body += "<td>" + data[i].name + "</td>";
-            body += "<td>" + data[i].triggers[0].name + "</td>";
-            body += "<td><img src='img/icons/working.png' width='50px' height='50px'</td>";
-            body += "<td><input type='checkbox' name='task' value='" + data[i].name + "'></td>";
-            body += "</tr>";
-        }
-        
-        body += "</tbody>";
-        body += "</table>";
-        document.getElementById("tasksTable").innerHTML = body;
-   })
-}
-
 function uploadJSON() {
-    // reset the field
-    jsonUpload.tasks = [];
-    // check which tasks should be executed and add them to JSON
-    var checkboxes = document.getElementsByName('task');
-    var selected = [];
-    
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            selected.push(checkboxes[i].value);
-        }
-    }
-    jsonUpload.tasks.push(selected);  
-    
     alert(JSON.stringify(jsonUpload));
-    
     // upload data to server
-    // TODO - gives 500 taskname not found
     $.ajax({
             url: 'http://localhost:8080/api/execute',
             type: 'post',
@@ -95,8 +88,6 @@ function uploadJSON() {
             success: function (data) {
                 
                 // TODO - get the result from server in callback
-                
-                alert(http.responseText);
             },
             data: jsonUpload
     });
