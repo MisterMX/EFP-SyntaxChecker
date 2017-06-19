aa230e22c0b541b76d87b6303bb69e2d9eac7faa
MisterMX <mbxd12@web.de>
2017-06-19 16:46:18 +0200

    Finish triggers for exercise 1

--- server/src/efp_syntaxchecker/tasks/task1.clj
+++ server/src/efp_syntaxchecker/tasks/task1.clj
@@ -4,16 +4,30 @@
   (:require
     [efp_syntaxchecker.tasks.taskutil :as taskutil]))
 
-(defn taskClassName [files]
+(defn triggerClassName [files]
   (boolean
     (re-find
       #"class\s+FileServer"
       (taskutil/removeJavaComments 
         (:content (taskutil/getFileByName "FileServer.java" files))))))
 
-(defn packageName [files]
+(defn triggerPackageName [files]
   (boolean
     (re-find
       #"package\s+var"
+      (taskutil/removeJavaComments 
+        (:content (taskutil/getFileByName "FileServer.java" files))))))
+
+(defn triggerProperties [files]
+  (boolean
+    (re-find
+      #"queue.files[ \t]+=[ \t]+\w+"
+      (taskutil/removeJavaComments 
+        (:content (taskutil/getFileByName "jndi.properties" files))))))
+
+(defn triggerTextMessage [files]
+  (boolean
+    (re-find
+      #"TextMessage"
       (taskutil/removeJavaComments 
         (:content (taskutil/getFileByName "FileServer.java" files))))))
