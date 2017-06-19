a10339e981b2d965cc39f865b05089aae2a1ca85
MisterMX <mbxd12@web.de>
2017-06-19 16:46:18 +0200

    Finish triggers for exercise 1

--- server/src/efp_syntaxchecker/config.clj
+++ server/src/efp_syntaxchecker/config.clj
@@ -9,11 +9,18 @@
     :name "Aufgabe 1"
     :triggers {
       :taskClassName {
-        :handler task1/taskClassName
+        :handler task1/triggerClassName
         :errorMsg "Class name must be 'FileServer'."}
       :packageName {
-        :handler task1/packageName
-        :errorMsg "Class must be in package 'var'."}}
+        :handler task1/triggerPackageName
+        :errorMsg "Class must be in package 'var' or in one of its subpackages."}
+      :properties {
+        :handler task1/triggerProperties
+        :errorMsg "Queue must be configured in file 'jndi.properties' with the key 'queue.files'."}
+      :textMessage {
+        :handler task1/triggerTextMessage
+        :errorMsg "Server must use the class 'TextMessage'"}
+      }
   }
   :task2 {
     :name "task2"
