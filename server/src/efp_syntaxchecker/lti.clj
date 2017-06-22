c1291dc5979577b7e87a53c78bac5d543832d93a
MisterMX <mbxd12@web.de>
2017-06-22 17:53:17 +0200

    Add connection to Moodle via LTI
    Use specialized library.

--- server/src/efp_syntaxchecker/lti.clj
+++ server/src/efp_syntaxchecker/lti.clj
@@ -1,20 +1,29 @@
 (ns efp_syntaxchecker.lti
+  (:import [org.imsglobal.pox IMSPOXRequest]
+           [org.apache.http.impl.client DefaultHttpClient]
+           [org.apache.commons.io IOUtils])
   (:use ring.util.response)
   (:require
     [efp_syntaxchecker.config :as config]))
 
 (defn createRedirect [params headers]
-  (println headers)
+  ; Send outcome request to Moodle and print response.
+  (println
+    (IOUtils/toString
+      (.getContent
+        (.getEntity
+          (.execute
+            (DefaultHttpClient.) 
+            (IMSPOXRequest/buildReplaceResult
+              (get params "lis_outcome_service_url")
+              "efpss17"
+              "efpss17"
+              (get params "lis_result_sourcedid")
+              "1.0"
+              nil
+              false))))))
   (redirect
     (str
       config/redirect-url-base
       "?lis_outcome_service_url=" (get params "lis_outcome_service_url") "&"
-      "lis_result_sourcedid=" (get params "lis_result_sourcedid") "&"
-      "lis_person_sourcedid=" (get params "list_person_sourcedid") "&"
-      "oauth_consumer_key=" (get params "oauth_consumer_key") "&"
-      "oauth_signature_method=" (get params "oauth_signature_method") "&"
-      "oauth_signature=" (get params "oauth_signature") "&"
-      "oauth_timestamp=" (get params "oauth_timestamp") "&"
-      "oauth_nonce=" (get params "oauth_nonce") "&"
-      "oauth_version=" (get params "oauth_version") "&"
-      "oauth_callback=" (get params "oauth_callback"))))
+      "lis_result_sourcedid=" (get params "lis_result_sourcedid"))))
